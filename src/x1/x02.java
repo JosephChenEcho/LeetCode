086077aa406de134fc2b433244f898968c128288
JosephChen432 <jochen@chcnetwork.org>
2016-08-31 15:55:11 -0700

    29 Divide 2 Integer

--- src/x1/x02.java
+++ src/x1/x02.java
@@ -12,7 +12,40 @@ import java.util.*;
 public class x02 {
     
     public static void main(String[] args){
+        divide(-2147483648,-1);
+    }
+    
+    //21 Merge Two Sorted Lists
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         
+        return null;        
+    }
+    
+    //29 Divide Two Integers
+    public static int divide(int dividend, int divisor) {
+        int sign = 1;
+        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
+            sign = -1;
+        long ldividend = Math.abs((long) dividend);
+        long ldivisor = Math.abs((long) divisor);
+
+        //Take care the edge cases.
+        if (ldivisor == 0) return Integer.MAX_VALUE;
+        if ((ldividend == 0) || (ldividend < ldivisor)) return 0;
+        
+        long result = 0;
+        while(ldividend >= ldivisor){
+            long subSum = ldivisor;
+            long count = 1;
+            while(subSum + subSum <= ldividend){
+                subSum += subSum;
+                count += count;
+            }
+            ldividend -= subSum;
+            result += count;
+        }
+        
+        return (int)result * sign;
     }
     
     //36 Valid Sudoku
