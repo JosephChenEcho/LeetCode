42b68aa69585c4546f2923d0c093a20939909ae1
JosephChen432 <jochen@chcnetwork.org>
2016-09-09 16:35:09 -0700

    13. Roman To Integer

--- src/x1/x01.java
+++ src/x1/x01.java
@@ -244,4 +244,33 @@ public class x01 {
         
         return maxarea;        
     }
+    
+    //12. Integer to Roman
+    public static String intToRoman(int num) {
+        if(num >= 1000) return "M" + intToRoman(num - 1000);
+        if(num >= 900) return "CM" + intToRoman(num - 900);
+        if(num >= 500) return "D" + intToRoman(num - 500);
+        if(num >= 400) return "CD" + intToRoman(num - 400);
+        if(num >= 100) return "C" + intToRoman(num - 100);
+        if(num >= 90) return "XC" + intToRoman(num - 90);
+        if(num >= 50) return "L" + intToRoman(num - 50);
+        if(num >= 40) return "XL" + intToRoman(num - 40);
+        if(num >= 10) return "X" + intToRoman(num - 10);
+        if(num >= 9) return "IX" + intToRoman(num - 9);
+        if(num >= 5) return "V" + intToRoman(num - 5);
+        if(num >= 4) return "IV" + intToRoman(num - 4);
+        if(num >= 1) return "I" + intToRoman(num - 1);
+        return "";
+    }
+    
+    //13. Roman to Integer
+    public static int romanToInt(String s) {
+        String[] preStrList = {"CM","CD","XC","XL","IX","IV"};
+        int[] preIntList = {900, 400, 90, 40, 9, 4};
+        
+        for(int i = 0; i < preStrList.length; i++){
+        
+        }
+        return 0;
+    }
 }
