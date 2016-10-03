25a03954c0ae9cc776fe204defdc3ed058a61c11
JosephChen432 <jochen@chcnetwork.org>
2016-10-03 14:57:30 -0700

    mock

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -5,6 +5,7 @@
  */
 package leetcode;
 
+import java.util.*;
 /**
  *
  * @author jochen
@@ -16,6 +17,7 @@ public class LeetCode {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //removeInvalidParentheses(")(");
     }
     
     public void setZeroes(int[][] matrix) {
@@ -44,4 +46,25 @@ public class LeetCode {
         
     }
     
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> ans = new ArrayList<>();
+        remove(s, ans, 0, 0, new char[]{'(', ')'});
+        return ans;
+    }
+    public void remove(String s, List<String> ans, int last_i, int last_j,  char[] par) {
+        for (int stack = 0, i = last_i; i < s.length(); ++i) {
+            if (s.charAt(i) == par[0]) stack++;
+            if (s.charAt(i) == par[1]) stack--;
+            if (stack >= 0) continue;
+            for (int j = last_j; j <= i; ++j)
+                if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]))
+                    remove(s.substring(0, j) + s.substring(j + 1, s.length()), ans, i, j, par);
+            return;
+        }
+        String reversed = new StringBuilder(s).reverse().toString();
+        if (par[0] == '(') // finished left to right
+            remove(reversed, ans, 0, 0, new char[]{')', '('});
+        else // finished right to left
+            ans.add(reversed);
+    }
 }
