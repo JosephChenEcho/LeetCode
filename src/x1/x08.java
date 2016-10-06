29cd1c4f0505b3382e3a5b304e3f428d740331cd
JosephChen432 <jochen@chcnetwork.org>
2016-10-05 17:01:57 -0700

    69

---
+++ src/x1/x08.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x1;
+
+/**
+ * Unsolved Hard:
+ * 84, 85, 97, 99
+ * @author jochen
+ */
+public class x08 {
+    public static void main(String[] args){
+    
+    }
+}
