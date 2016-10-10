017a61ae2583329670b8ca36c5d3831bdf44e387
JosephChen432 <jochen@chcnetwork.org>
2016-10-10 10:11:54 -0700

    83. Remove Duplicates from Sorted List

--- src/x1/x08.java
+++ src/x1/x08.java
@@ -12,7 +12,17 @@ package x1;
  */
 public class x08 {
     public static void main(String[] args){
-    
+        ListNode input1 = new ListNode(1);
+        ListNode input2 = new ListNode(1);
+        ListNode input3 = new ListNode(2);
+        ListNode input4 = new ListNode(2);
+        ListNode input5 = new ListNode(2);
+        input1.next = input2;
+        input2.next = input3;
+        input3.next = input4;
+        input4.next = input5;
+        System.out.println(input1.toString());
+        System.out.println(deleteDuplicates(input1).toString());
     }
     
     //81. Search in Rotated Sorted Array II
@@ -26,7 +36,15 @@ public class x08 {
     }
     
     //83. Remove Duplicates from Sorted List
-    public ListNode deleteDuplicates(ListNode head) {
-        return null;
+    public static ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode current = head;
+        while(current != null && current.next != null){
+            while(current.next != null && current.val == current.next.val){
+                current.next = current.next.next;
+            }        
+            current = current.next;
+        }        
+        return head;
     }
 }
