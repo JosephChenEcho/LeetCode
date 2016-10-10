994da201391538b3afc94a8f2d60e45a4af73de6
Joseph <Joseph@192.168.2.12>
2016-10-09 21:29:55 -0700

    81

--- src/x1/x08.java
+++ src/x1/x08.java
@@ -14,4 +14,19 @@ public class x08 {
     public static void main(String[] args){
     
     }
+    
+    //81. Search in Rotated Sorted Array II
+    public boolean search(int[] nums, int target) {
+        return false;
+    }
+    
+    //82. Remove Duplicates from Sorted List II
+    public ListNode deleteDuplicates2(ListNode head) {
+        return null;
+    }
+    
+    //83. Remove Duplicates from Sorted List
+    public ListNode deleteDuplicates(ListNode head) {
+        return null;
+    }
 }
