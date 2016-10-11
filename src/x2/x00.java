4972eae14fd6b002928bbf62bea8eb5be1389829
JosephChen432 <jochen@chcnetwork.org>
2016-10-11 16:47:03 -0700

    init 100+

---
+++ src/x2/x00.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x2;
+
+/**
+ * Unsolved Hard:
+ * 115, 117
+ * @author jochen
+ */
+public class x00 {
+    public static void main(String args[]){
+    
+    }
+}
