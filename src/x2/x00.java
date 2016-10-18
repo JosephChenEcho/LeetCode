0ac583ca299b95b6c854fd19cb2d3a787781d009
JosephChen432 <jochen@chcnetwork.org>
2016-10-18 09:55:36 -0700

    112. Path Sum

--- src/x2/x00.java
+++ src/x2/x00.java
@@ -171,4 +171,14 @@ public class x00 {
         root.right = buildBST(mid+1, high, nums);
         return root;
     }
+    
+    //112. Path Sum
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null){
+            if(root.val == sum) return true;
+            return false;
+        }        
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
 }
