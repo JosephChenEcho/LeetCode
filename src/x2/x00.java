131a3f0c1a6384a801ded5b79259c6a6899c280b
Joseph <Joseph@192.168.2.12>
2016-10-19 21:39:13 -0700

    120. Triangle

--- src/x2/x00.java
+++ src/x2/x00.java
@@ -361,4 +361,29 @@ public class x00 {
     }
     
     //120. Triangle
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[][] dp = new int[triangle.size()][];
+        for (int i = 0; i < triangle.size(); i++) {
+            dp[i] = new int[triangle.get(i).size()];
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = triangle.get(i).get(j);
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + triangle.get(i).get(j);
+                } else if (j == triangle.get(i).size() - 1) {
+                    dp[i][j] = dp[i - 1][j - 1] + triangle.get(i).get(j);
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + triangle.get(i).get(j), dp[i - 1][j] + triangle.get(i).get(j));
+                }
+            }
+        }
+ 
+        int min = Integer.MAX_VALUE;
+        for (int num : dp[triangle.size() - 1]) {
+            if (num < min) {
+                min = num;
+            }
+        }
+        return min;
+    }
 }
