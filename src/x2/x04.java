ca36f4442c9c88beadad8e4acde9854de4cc6d9d
Joseph <Joseph@192.168.2.12>
2016-10-23 21:22:59 -0700

    141. Linked List Cycle

--- src/x2/x04.java
+++ src/x2/x04.java
@@ -13,11 +13,21 @@ import java.util.*;
  */
 public class x04 {
     public static void main(String args[]){
-    
+        ListNode input1 = new ListNode(1);
+        ListNode input2 = new ListNode(2);
+        ListNode input3 = new ListNode(3);
+        ListNode input4 = new ListNode(4);
+        ListNode input5 = new ListNode(5);
+        input1.next = input2;
+        input2.next = input3;
+        input3.next = input4;
+        input4.next = input5;
+        input5.next = input2;
+        hasCycle(input1);
     }
     
     //141. Linked List Cycle
-    public boolean hasCycle(ListNode head) {
+    public static boolean hasCycle(ListNode head) {
         if(head==null)return false;
         while(head.next!=null && head.next!=head){
             ListNode tmp=head.next;
