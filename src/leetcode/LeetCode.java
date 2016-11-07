77455e75ce0aceb127269d80712de358568d9af1
Joseph <Joseph@192.168.2.12>
2016-11-06 16:16:57 -0800

    mock interview

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -19,8 +19,11 @@ public class LeetCode {
         // TODO code application logic here
         //int[][] input = new int[][]{{0,0,1,1},{0,1,3,2},{1,0,2,2}};
         //isRectangleCover(input);
-        int[][] input = new int[][]{{1,-3,3},{0,-2,0},{-3,-3,-3}};
-        calculateMinimumHP(input);
+        //int[][] input = new int[][]{{1,-3,3},{0,-2,0},{-3,-3,-3}};
+        //calculateMinimumHP(input);
+        int[] input = new int[]{10,12,21,4,6,78,9,91,34,45,27};
+        System.out.println(maxProfit2(input));
+        System.out.println(maxProfit(3,input));
         
     }
     
@@ -174,6 +177,74 @@ public class LeetCode {
         }        
         return dungeon[0][0];
     }   
+    
+    public static int maxProfit2(int[] prices) {
+        int max = 0;
+        boolean buy = false;
+        int buyPrice = Integer.MAX_VALUE;
+        for(int i = 0; i < prices.length - 1; i++){
+            if(!buy && prices[i] < prices[i + 1]){
+                //buy stock
+                buyPrice = prices[i];
+                buy = true;
+                continue;
+            }
+            if(buy && prices[i] > prices[i + 1]){
+                max += prices[i] - buyPrice;
+                buy = false;
+            }
+        } 
+        if(buy && prices[prices.length - 1] > buyPrice){
+            max += prices[prices.length - 1] - buyPrice;
+        }
+        return max;
+    }
+    
+    public static int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        if(n <= 1) return 0;
+        
+        
+        /*int[][] dp = new int[k+1][n];
+        for (int i = 1; i <= k; i++) {
+            int localMax = dp[i-1][0] - prices[0];
+            for (int j = 1; j < n; j++) {
+                    dp[i][j] = Math.max(dp[i][j-1],  prices[j] + localMax);
+                    localMax = Math.max(localMax, dp[i-1][j] - prices[j]);
+                    System.out.print(localMax+"\t");
+            }
+            System.out.println();
+        }
+        //{10,12,21,4,6,78,9,91,34,45,27};
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[i].length; j++){
+                System.out.print(dp[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        return dp[k][n-1];*/
+        if (k >=  n/2) {
+		int maxPro = 0;
+		for (int i = 1; i < n; i++) {
+			if (prices[i] > prices[i-1])
+				maxPro += prices[i] - prices[i-1];
+		}
+		return maxPro;
+	}
+        int[] dp = new int[n];
+        int trans =  0;
+        while(trans < k){
+            int localMax = -prices[0];
+            for(int i = 1; i < n; i++){
+                int tmp = Math.max(localMax, dp[i] - prices[i]);
+                dp[i] = Math.max(dp[i - 1], prices[i] + localMax);
+                localMax = tmp;
+            }    
+            
+            trans++;
+        }
+        return dp[n - 1];
+    }
 }
 
 
