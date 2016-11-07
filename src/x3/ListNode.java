318bd88cf315721a0ee230ec42088a07a5a37c41
JosephChen432 <jochen@chcnetwork.org>
2016-11-07 11:22:09 -0800

    init x3

---
+++ src/x3/ListNode.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x3;
+
+/**
+ *
+ * @author jochen
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x){ val = x; }
+    
+    public String toString(){
+        String retstr = "";
+        ListNode current = this;
+        while(current != null){
+            retstr += current.val + "\t";
+            current = current.next;
+        }
+        return retstr;
+    }
+}
---
+++ src/x3/x00.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x3;
+
+/**Unsolved Hard:
+ * 212, 214, 218
+ * 
+ * @author jochen
+ */
+public class x00 {
+    public static void main(String args[]){
+    
+    }
+    //202. Happy Number
+    public boolean isHappy(int n) {
+        return true;
+    }
+    
+    //203. Remove Linked List Elements
+    public ListNode removeElements(ListNode head, int val) {
+        return null;
+    }
+    
+    //204. Count Primes
+    public int countPrimes(int n) {
+        return -1;
+    }
+}
