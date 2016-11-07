4e3d8d37bb92cc8380cb03535ed95cd575882cb2
JosephChen432 <jochen@chcnetwork.org>
2016-11-07 11:34:34 -0800

    202. Happy Number

--- src/x3/x00.java
+++ src/x3/x00.java
@@ -5,6 +5,8 @@
  */
 package x3;
 
+import java.util.*;
+
 /**Unsolved Hard:
  * 212, 214, 218
  * 
@@ -12,10 +14,32 @@ package x3;
  */
 public class x00 {
     public static void main(String args[]){
-    
+        int input = 1329;
+        while(input != 1){
+            int ret = 0;
+            while(input != 0){
+                int mod = input % 10;
+                ret += mod * mod;
+                input /= 10;
+            }
+            System.out.println("ret = " + ret);
+            input = ret;
+        }
     }
     //202. Happy Number
     public boolean isHappy(int n) {
+        HashSet<Integer> intSet = new HashSet();
+        while(n != 1){
+            int ret = 0;
+            while(n != 0){
+                int mod = n % 10;
+                ret += mod * mod;
+                n /= 10;
+            }
+            
+            n = ret;
+            if(!intSet.add(n)) return false;
+        }
         return true;
     }
     
