68bc951fb759208986aab79b2c8549e22751ed4b
JosephChen432 <jochen@chcnetwork.org>
2016-11-10 09:33:30 -0800

    209. Minimum Size Subarray Sum

--- src/x3/x00.java
+++ src/x3/x00.java
@@ -14,7 +14,7 @@ import java.util.*;
  */
 public class x00 {
     public static void main(String args[]){
-        minSubArrayLen(4, new int[]{2,3,1,2,4,3});
+        minSubArrayLen(7, new int[]{2,3,1,2,4,3});
     }
     //201
     
@@ -160,47 +160,82 @@ public class x00 {
     
     //209. Minimum Size Subarray Sum
     public static int minSubArrayLen(int s, int[] nums) {
-        /*int min = Integer.MAX_VALUE;
-        int sum = 0;
-        for(int i = 0; i < nums.length; i++){
-            sum = 0;
-            for(int j = 0; j + i < nums.length;j++){
-                sum += nums[j + i];
-                if(sum >= s){
-                    min = Math.min(min, j + 1);
-                    break;
-                }
-            }            
-        }        
-        return min == Integer.MAX_VALUE ? 0 : min;*/
         int start = 0;
         int end = 0;
         int sum = 0;
-        int min;
-        while(sum < s && end < nums.length){
-            sum += nums[end++];
-        }
-        if(end == nums.length) return 0;
-        min = end;
-        //end--;
-        while (!(end == nums.length && sum < s)){
-            //init
-            while(sum >= s){
-                sum -= nums[start++];
-            }
-            start--;
-            int tmp = end - start;
-            min = Math.min(min, tmp);
+        int min = Integer.MAX_VALUE;
+        while(end < nums.length){
             while(sum < s && end < nums.length){
                 sum += nums[end++];
             }
-        }
-        
-        return min;
+            while(sum >= s){
+                min = Math.min(min, end - start);
+                sum -= nums[start++];
+            }
+        }       
+        return min == Integer.MAX_VALUE ? 0 : min;
     }
     
     //210. Course Schedule II
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        return null;
+        int[] incLinkCounts = new int[numCourses];
+        List<List<Integer>> adjs = new ArrayList<>(numCourses);
+        initialiseGraph(incLinkCounts, adjs, prerequisites);
+        //return solveByBFS(incLinkCounts, adjs);
+        return solveByDFS(adjs);
+    }
+    private void initialiseGraph(int[] incLinkCounts, List<List<Integer>> adjs, int[][] prerequisites){
+        int n = incLinkCounts.length;
+        while (n-- > 0) adjs.add(new ArrayList<>());
+        for (int[] edge : prerequisites) {
+            incLinkCounts[edge[0]]++;
+            adjs.get(edge[1]).add(edge[0]);
+        }
+    }
+    
+    private int[] solveByBFS(int[] incLinkCounts, List<List<Integer>> adjs){
+        int[] order = new int[incLinkCounts.length];
+        Queue<Integer> toVisit = new ArrayDeque<>();
+        for (int i = 0; i < incLinkCounts.length; i++) {
+            if (incLinkCounts[i] == 0) toVisit.offer(i);
+        }
+        int visited = 0;
+        while (!toVisit.isEmpty()) {
+            int from = toVisit.poll();
+            order[visited++] = from;
+            for (int to : adjs.get(from)) {
+                incLinkCounts[to]--;
+                if (incLinkCounts[to] == 0) toVisit.offer(to);
+            }
+        }
+        return visited == incLinkCounts.length ? order : new int[0]; 
+    }
+    
+    private int[] solveByDFS(List<List<Integer>> adjs) {
+        BitSet hasCycle = new BitSet(1);
+        BitSet visited = new BitSet(adjs.size());
+        BitSet onStack = new BitSet(adjs.size());
+        Deque<Integer> order = new ArrayDeque<>();
+        for (int i = adjs.size() - 1; i >= 0; i--) {
+            if (visited.get(i) == false && hasOrder(i, adjs, visited, onStack, order) == false) return new int[0];
+        }
+        int[] orderArray = new int[adjs.size()];
+        for (int i = 0; !order.isEmpty(); i++) orderArray[i] = order.pop();
+        return orderArray;
+    }
+
+    private boolean hasOrder(int from, List<List<Integer>> adjs, BitSet visited, BitSet onStack, Deque<Integer> order) {
+        visited.set(from);
+        onStack.set(from);
+        for (int to : adjs.get(from)) {
+            if (visited.get(to) == false) {
+                if (hasOrder(to, adjs, visited, onStack, order) == false) return false;
+            } else if (onStack.get(to) == true) {
+                return false;
+            }
+        }
+        onStack.clear(from);
+        order.push(from);
+        return true;
     }
 }
