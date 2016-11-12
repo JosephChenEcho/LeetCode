296664024810680490234d0aafa886f444697fe0
JosephChen432 <jochen@chcnetwork.org>
2016-11-11 16:36:18 -0800

    220. Contains Duplicate III

--- src/x3/x00.java
+++ src/x3/x00.java
@@ -328,17 +328,46 @@ public class x00 {
     }
     
     //218. The Skyline Problem
-    public List<int[]> getSkyline(int[][] buildings) {
+    public List<int[]> getSkyline(int[][] buildings) {    
+        //buildings [Li, Ri, Hi]
+        for(int i = 0; i < buildings.length; i++){}
         return null;
     }
     
     //219. Contains Duplicate II
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> iMap = new HashMap();
+        for(int i = 0; i < nums.length; i++){
+            if(iMap.containsKey(nums[i])){
+                if(k >= i - iMap.get(nums[i])) return true;
+            }
+            iMap.put(nums[i], i);
+        }
         return false;
     }
     
     //220. Contains Duplicate III
+      /*Given an array of integers, find out whether there are two distinct indices i and j in the array
+        such that the difference between nums[i] and nums[j] is at most t and the difference
+        between i and j is at most k.*/
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (t < 0) return false;
+        Map<Long, Long> d = new HashMap<>();
+        long w = (long)t + 1;
+        for (int i = 0; i < nums.length; ++i) {
+            long m = getID(nums[i], w);
+            if (d.containsKey(m))
+                return true;
+            if (d.containsKey(m - 1) && Math.abs(nums[i] - d.get(m - 1)) < w)
+                return true;
+            if (d.containsKey(m + 1) && Math.abs(nums[i] - d.get(m + 1)) < w)
+                return true;
+            d.put(m, (long)nums[i]);
+            if (i >= k) d.remove(getID(nums[i - k], w));
+        }
         return false;
+    }
+    private long getID(long i, long w) {
+        return i < 0 ? (i + 1) / w - 1 : i / w;
     }
 }
