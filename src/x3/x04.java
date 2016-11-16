951a0fc70c54625b6a25d0bd6bda45bba09a436f
JosephChen432 <jochen@chcnetwork.org>
2016-11-15 16:48:49 -0800

    232. Implement Queue using Stacks

---
+++ src/x3/x04.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x3;
+import java.util.*;
+/**
+ *
+ * @author jochen
+ */
+public class x04 {
+    public static void main(String[] args){
+    
+    }
+    
+    //241. Different Ways to Add Parentheses
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> retList = new ArrayList();
+        return null;
+    }
+}
