63748a38a6aee805788e85d8da0ea508c8fcadc4
JosephChen432 <jochen@chcnetwork.org>
2016-12-02 16:58:13 -0800

    240. Search a 2D Matrix II

--- src/x3/x02.java
+++ src/x3/x02.java
@@ -290,7 +290,40 @@ public class x02 {
         node.next = node.next.next;
     }
     
-    //238
+    //238. Product of Array Except Self
+    //For example, given [1,2,3,4], return [24,12,8,6].
+    public int[] productExceptSelf(int[] nums) {
+        return null;
+    }
     
-    //240
+    //240. Search a 2D Matrix II
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if(m<1) return false;
+        int n = matrix[0].length;
+
+        return searchMatrix(matrix, new int[]{0,0}, new int[]{m-1, n-1}, target);
+    }
+
+    private boolean searchMatrix(int[][] matrix, int[] upperLeft, int[] lowerRight, int target) {
+	if(upperLeft[0]>lowerRight[0] || upperLeft[1]>lowerRight[1]
+			|| lowerRight[0]>=matrix.length || lowerRight[1]>=matrix[0].length) 
+		return false;
+	if(lowerRight[0]-upperLeft[0]==0 && lowerRight[1]-upperLeft[1]==0)
+		return matrix[upperLeft[0]][upperLeft[1]] == target;
+	int rowMid = (upperLeft[0] + lowerRight[0]) >> 1;
+	int colMid = (upperLeft[1] + lowerRight[1]) >> 1;
+	int diff = matrix[rowMid][colMid] - target;
+	if(diff > 0) {
+		return searchMatrix(matrix, upperLeft, new int[]{rowMid, colMid}, target)
+				|| searchMatrix(matrix, new int[]{upperLeft[0],colMid+1}, new int[]{rowMid, lowerRight[1]}, target)
+				|| searchMatrix(matrix, new int[]{rowMid+1,upperLeft[1]}, new int[]{lowerRight[0], colMid}, target);
+	}
+	else if(diff < 0) {
+ 		return searchMatrix(matrix, new int[]{upperLeft[0], colMid+1}, new int[]{rowMid, lowerRight[1]}, target)
+				|| searchMatrix(matrix, new int[]{rowMid+1, upperLeft[1]}, new int[]{lowerRight[0], colMid}, target)
+				|| searchMatrix(matrix, new int[]{rowMid+1, colMid+1}, lowerRight, target);
+	}
+	else return true;
+    }
 }
