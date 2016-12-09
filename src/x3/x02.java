ab52e5c0f483a5820edece7cbce6abd6ce738882
JosephChen432 <jochen@chcnetwork.org>
2016-12-09 14:40:01 -0800

    238. Product of Array Except Self

--- src/x3/x02.java
+++ src/x3/x02.java
@@ -293,7 +293,18 @@ public class x02 {
     //238. Product of Array Except Self
     //For example, given [1,2,3,4], return [24,12,8,6].
     public int[] productExceptSelf(int[] nums) {
-        return null;
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
     }
     
     //240. Search a 2D Matrix II
