324f081b6725ccc83f75f961f03666fc2d107e9c
JosephChen432 <jochen@chcnetwork.org>
2016-12-09 16:52:54 -0800

    241

--- src/x3/x04.java
+++ src/x3/x04.java
@@ -17,6 +17,41 @@ public class x04 {
     //241. Different Ways to Add Parentheses
     public List<Integer> diffWaysToCompute(String input) {
         List<Integer> retList = new ArrayList();
+        char[] carr = input.trim().toCharArray();
+        List<Integer> iList = new ArrayList();
+        List<Character> cList = new ArrayList();        
+        int ival = 0;
+        for(int i = 0; i < carr.length; i++){
+            if(carr[i] == ' ') continue;
+            if(carr[i] - '0' >= 0 && carr[i] - '0' <= 9){
+                ival = ival * 10 + (carr[i] - '0');
+            }else{
+                iList.add(ival);
+                ival = 0;
+                cList.add(carr[i]);
+            }
+        }        
+        iList.add(ival);
+        //only one number
+        if(cList.isEmpty()){
+            retList.add(ival);
+            return retList;
+        }
+        
+        retList.addAll(compute(iList, cList, 0, iList.size() - 1));
+        
+        return null;
+    }
+    
+    public List<Integer> compute(List<Integer> iList, List<Character> cList, int istart, int iend){
+        List<Integer> retList = new ArrayList();
+        if(istart == iend){
+            retList.add(iList.get(istart));
+            return retList;
+        }
+        for(int i = istart; i <= iend; i++){
+            
+        }
         return null;
     }
 }
