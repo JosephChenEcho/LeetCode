725aed805d6a9e6ca444c20d430603619e78b997
Joseph <Joseph@192.168.2.12>
2016-12-10 18:14:35 -0800

    242. Valid Anagram

--- src/x3/x04.java
+++ src/x3/x04.java
@@ -72,4 +72,13 @@ public class x04 {
         }
         return retList;
     }
+    
+    //242. Valid Anagram
+    public boolean isAnagram(String s, String t) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
+        return true;
+    }
 }
