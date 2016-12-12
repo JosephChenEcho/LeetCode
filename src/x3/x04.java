55cfb09cb074ed5e1be8c149a738b993b2c3980d
Joseph <Joseph@192.168.2.12>
2016-12-11 22:35:01 -0800

    258. Add Digits

--- src/x3/x04.java
+++ src/x3/x04.java
@@ -129,4 +129,31 @@ public class x04 {
         return num;*/
         return num==0?0:(num%9==0?9:(num%9));
     }
+    
+    //260. Single Number III
+    public int[] singleNumber(int[] nums) {
+        // Pass 1 : 
+        // Get the XOR of the two numbers we need to find
+        int diff = 0;
+        for (int num : nums) {
+            diff ^= num;
+        }
+        // Get its last set bit
+        diff &= -diff;
+        
+        // Pass 2 :
+        int[] rets = {0, 0}; // this array stores the two numbers we will return
+        for (int num : nums)
+        {
+            if ((num & diff) == 0) // the bit is not set
+            {
+                rets[0] ^= num;
+            }
+            else // the bit is set
+            {
+                rets[1] ^= num;
+            }
+        }
+        return rets;
+    }
 }
