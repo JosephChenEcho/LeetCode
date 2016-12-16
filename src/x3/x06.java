54a7ec45d43fb95163d9c2a12e040bd9c005b52e
Joseph <Joseph@192.168.2.12>
2016-12-15 21:06:43 -0800

    263. Ugly Number

---
+++ src/x3/x06.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x3;
+
+/**
+ *
+ * @author Joseph
+ */
+public class x06 {
+    public static void main(String[] args){
+            
+    }
+    
+    //263. Ugly Number
+    public boolean isUgly(int num) {
+        while(num%2==0&&num!=0){
+            num /=2;
+        }
+        while(num%3==0&&num!=0){
+            num /=3;
+        }
+        while(num%5==0&&num!=0){
+            num /=5;
+        }
+        if(num == 1) return true; 
+        return false;
+    }
+}
