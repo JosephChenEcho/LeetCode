64233dd83964613312f47cbfe936f149307e1014
JosephChen432 <jochen@chcnetwork.org>
2016-12-23 13:57:14 -0800

    264. Ugly Number II

--- src/x3/x06.java
+++ src/x3/x06.java
@@ -31,12 +31,22 @@ public class x06 {
     
     //264. Ugly Number II
     public int nthUglyNumber(int n) {
-        int i = 0;
-        int num = 0;
-        while(i < n){
-            
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        int index2 = 0, index3 = 0, index5 = 0;
+        int factor2 = 2, factor3 = 3, factor5 = 5;
+        for(int i=1;i<n;i++){
+            int min = Math.min(Math.min(factor2,factor3),factor5);
+            ugly[i] = min;
+            if(factor2 == min)
+                factor2 = 2*ugly[++index2];
+            if(factor3 == min)
+                factor3 = 3*ugly[++index3];
+            if(factor5 == min)
+                factor5 = 5*ugly[++index5];
         }
-        
-        return -1;
+        return ugly[n-1];
     }
+    
+    //
 }
