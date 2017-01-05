974d97ff629358e4574d9d955bf98b1200fbd316
JosephChen432 <jochen@chcnetwork.org>
2017-01-04 16:28:52 -0800

    274. H-Index

--- src/x3/x06.java
+++ src/x3/x06.java
@@ -61,9 +61,27 @@ public class x06 {
     
     //274. H-Index
     public int hIndex(int[] citations) {
-        if(citations.length == 0) return 0;
-        Arrays.sort(citations);        
-        return citations[citations.length/2];
+        int length = citations.length;
+        if (length == 0) {
+            return 0;
+        }        
+        int[] array2 = new int[length + 1];
+        for (int i = 0; i < length; i++) {
+            if (citations[i] > length) {
+                array2[length] += 1;
+            } else {
+                array2[citations[i]] += 1;
+            }
+        }
+        int t = 0;
+
+        for (int i = length; i >= 0; i--) {
+            t += array2[i];
+            if (t >= i) {
+                return i;
+            }
+        }
+        return 0;
     }
     
     //275. H-Index II
