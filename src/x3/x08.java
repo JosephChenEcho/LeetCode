33091d252e8caf756ee1cfb61ba2ad6597af9f2e
JosephChen432 <jochen@chcnetwork.org>
2017-01-09 16:07:36 -0800

    X08

---
+++ src/x3/x08.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x3;
+
+/**
+ *
+ * @author jochen
+ */
+public class x08 {
+    public static void main(String[] args){
+    
+    }
+    
+    //283. Move Zeroes
+    public void moveZeroes(int[] nums) {
+        
+    }
+}
