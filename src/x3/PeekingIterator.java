83a91d9162b4b49f207970896084a68dc43ec5f1
JosephChen432 <jochen@chcnetwork.org>
2017-01-10 16:53:05 -0800

    284. Peeking Iterator

--- src/x3/PeekingIterator.java
+++ src/x3/PeekingIterator.java
@@ -11,25 +11,33 @@ import java.util.*;
  */
 //284. Peeking Iterator
 public class PeekingIterator implements Iterator<Integer>{
+    
+    private Iterator<Integer> iter = null;
+    private Integer next = null;
+    
     public PeekingIterator(Iterator<Integer> iterator) {
-	    // initialize any member here.
-	    
-	}
+        iter = iterator;
+        if(iter.hasNext()){
+            next = iter.next();
+        }            
+    }
 
     // Returns the next element in the iteration without advancing the iterator.
-	public Integer peek() {
-            return -1;
-	}
+    public Integer peek() {
+        return next;
+    }
 
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	@Override
 	public Integer next() {
-	    return -1;
+            Integer retnext = next;
+            next = iter.hasNext() ? iter.next() : null;
+	    return retnext;
 	}
 
 	@Override
 	public boolean hasNext() {
-            return false;
+            return next != null;
         }	    
 }
