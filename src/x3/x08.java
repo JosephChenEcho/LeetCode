f9367043b6edcc2a12618e39d713eb453e22e883
JosephChen432 <jochen@chcnetwork.org>
2017-01-12 14:43:01 -0800

    289. Game of Life

--- src/x3/x08.java
+++ src/x3/x08.java
@@ -11,7 +11,14 @@ package x3;
  */
 public class x08 {
     public static void main(String[] args){
-    
+        int[][] input = {{1,1},{1,0}};
+        gameOfLife(input);
+        for(int[] ai : input){
+            for(int i : ai){
+                System.out.print(i + "\t");
+            }
+            System.out.println();
+        }
     }
     
     //283. Move Zeroes
@@ -35,7 +42,43 @@ public class x08 {
     }
     
     //289. Game of Life
-    public void gameOfLife(int[][] board) {
-        
+    public static void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                dieOrLive(i, j, board, m, n);
+            }
+        }        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                board[i][j] /= 2;
+            }
+        }    
+    }
+    
+    public static void dieOrLive(int i, int j, int[][] board, int m, int n){
+        int count = 0;
+        if(board[i][j] == 1){
+            if(i > 0 && board[i-1][j]%2 == 1) count++;
+            if(i < m - 1 && board[i+1][j]%2 == 1) count++;
+            if(j > 0 && board[i][j-1]%2 == 1) count++;
+            if(j < n - 1 && board[i][j+1]%2 == 1) count++;
+            if(i > 0 && j > 0 && board[i-1][j-1]%2 == 1) count++;
+            if(i > 0 && j < n - 1 && board[i-1][j+1]%2 == 1) count++;
+            if(i < m - 1 && j > 0 && board[i+1][j-1]%2 == 1) count++;
+            if(i < m - 1 && j < n - 1 && board[i+1][j+1]%2 == 1) count++;
+            if(count == 2 || count == 3) board[i][j] += 2;
+        }else{
+            if(i > 0 && board[i-1][j]%2 == 1) count++;
+            if(i < m - 1 && board[i+1][j]%2 == 1) count++;
+            if(j > 0 && board[i][j-1]%2 == 1) count++;
+            if(j < n - 1 && board[i][j+1]%2 == 1) count++;
+            if(i > 0 && j > 0 && board[i-1][j-1]%2 == 1) count++;
+            if(i > 0 && j < n - 1 && board[i-1][j+1]%2 == 1) count++;
+            if(i < m - 1 && j > 0 && board[i+1][j-1]%2 == 1) count++;
+            if(i < m - 1 && j < n - 1 && board[i+1][j+1]%2 == 1) count++;
+            if(count == 3) board[i][j] = 2;
+        }
     }
 }
