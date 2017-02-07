1dee1ff5d77e7d47ded577f6ea033b22f155c77c
JosephChen432 <jochen@chcnetwork.org>
2017-02-06 16:49:14 -0800

    322. Coin Change

---
+++ src/x4/x02.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x4;
+import java.util.*;
+/**
+ *
+ * @author jochen
+ */
+public class x02 {
+    public static void main(String[] args){
+    
+    }
+    //322. Coin Change
+    public int coinChange(int[] coins, int amount) {        
+        return -1;
+    }
+}
