d7dffb062d4490d8632a4594ffb7005804bd3e35
Joseph <Joseph@192.168.2.12>
2017-02-06 21:47:37 -0800

    322. Coin Change

--- src/x4/x02.java
+++ src/x4/x02.java
@@ -11,10 +11,23 @@ import java.util.*;
  */
 public class x02 {
     public static void main(String[] args){
+        int[] input = {1,3,7,5,4};
+        Arrays.sort(input);
+        for(int i : input){
+            System.out.println(i);
+        }
     
     }
     //322. Coin Change
-    public int coinChange(int[] coins, int amount) {        
+    public int coinChange(int[] coins, int amount) {     
+        Arrays.sort(coins);
+        
         return -1;
+    }
+    
+    public int coinChange(int[] coins, int amount, int idx){
+        if(coins[0] > amount) return -1;
+        if(coins[idx] == amount) return 1;
+        return 0;
     }
 }
