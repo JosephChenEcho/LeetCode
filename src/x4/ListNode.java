cedc6755398a952e1d1dac94878e7ef290bf611d
JosephChen432 <jochen@chcnetwork.org>
2017-02-08 11:54:46 -0800

    ListNode

--- src/x4/ListNode.java
+++ src/x4/ListNode.java
@@ -6,7 +6,15 @@
 package x4;
 
 /**
- *
+ *ListNode input1 = new ListNode(1);
+        ListNode input2 = new ListNode(2);
+        ListNode input3 = new ListNode(3);
+        ListNode input4 = new ListNode(4);
+        ListNode input5 = new ListNode(5);
+        input1.next = input2;
+        input2.next = input3;
+        input3.next = input4;
+        input4.next = input5;
  * @author jochen
  */
 public class ListNode {
