7ae2964933aa32604f289971d8384bfdd476479d
Joseph <Joseph@192.168.2.12>
2017-02-08 23:17:13 -0800

    331. Verify Preorder Serialization of a Binary Tree

--- src/x4/x02.java
+++ src/x4/x02.java
@@ -103,6 +103,12 @@ public class x02 {
     
     //331. Verify Preorder Serialization of a Binary Tree
     public boolean isValidSerialization(String preorder) {
-        return false;
+        String[] nodes = preorder.split(",");
+        int diff = 1;
+        for (String node: nodes) {
+            if (--diff < 0) return false;
+            if (!node.equals("#")) diff += 2;
+        }
+        return diff == 0;
     }
 }
