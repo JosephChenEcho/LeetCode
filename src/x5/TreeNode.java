44423deaf8ea761a0b92f71786587bf031b25026
JosephChen432 <jochen@chcnetwork.org>
2017-02-16 17:04:14 -0800

    450. Delete Node in a BST

---
+++ src/x5/TreeNode.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x5;
+
+import x4.*;
+
+/**
+ *
+ * @author jochen
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val = x;
+    }
+}
---
+++ src/x5/x04.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x5;
+
+/**
+ *
+ * @author jochen
+ */
+public class x04 {
+    public static void main(String[] args){}
+    
+    //450. Delete Node in a BST
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return null;
+    }
+}
