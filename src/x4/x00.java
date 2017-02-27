d0af1e514834a0e28a25a4b4720ca36a2ce24635
Joseph <Joseph@192.168.2.12>
2017-02-26 23:30:52 -0800

    315. Count of Smaller Numbers After Self

--- src/x4/x00.java
+++ src/x4/x00.java
@@ -179,7 +179,7 @@ public class x00 {
         return retList;
         //26,78,27,100,33,67,90,23,66,5,38,7,35,23,52,22,83,51,98,69,81,32,78,28,94,13,2,97,3,76,99,51,9,21,84,66,65,36,100,41
         //[10,27,10,35,12,22,28,8,19,2,12,2,9,6,12,5,17,9,19,12,14,6,12,5,12,3,0,10,0,7,8,4,0,0,4,3,2,0,1,0]
-        //[10,00,10,18,12,22,28,0,00,2,12,2,9,6,12,5,17,0,19,12,14,6,12,5,12,3,0,10,0,7,8,4,0,0,4,3,2,0,1,0]
+        //[18,46,18,17,12,41,47,8,19,2,12,2,9,6,21,5,17,9,19,12,14,6,12,5,12,3,0,10,0,7,8,4,0,0,4,3,2,0,1,0]
     }
     
     public static int insert(SumNode root, int val){
@@ -190,11 +190,17 @@ public class x00 {
         while(true){
             //dup case
             if(point.val == val){
+                if(point.val == 51){
+                    System.out.println("here");
+                }
                 point.dup += 1;
-                count = point.count;
+                point.count = count;
                 break;
             }
             else if(val < point.val){
+                if(point.val == 51){
+                    System.out.println("here");
+                }
                 point.count += 1;
                 if(point.smaller == null) break;
                 point = point.smaller;
