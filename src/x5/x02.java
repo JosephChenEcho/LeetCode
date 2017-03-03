49199e69a5e39460459a9286684673b71f8879ab
Joseph <Joseph@192.168.2.12>
2017-03-02 21:31:05 -0800

    422. Valid Word Square

--- src/x5/x02.java
+++ src/x5/x02.java
@@ -4,14 +4,33 @@
  * and open the template in the editor.
  */
 package x5;
-
+import java.util.*;
 /**
  *
  * @author Joseph
  */
 public class x02 {
     
+    public static void main(String[] args){
+    
+    }
+    
+    //422. Valid Word Square
+    public boolean validWordSquare(List<String> words) {
+        if(words == null || words.size() == 0){
+            return true;
+        }
+        int n = words.size();
+        for(int i=0; i<n; i++){
+            for(int j=i; j<words.get(i).length(); j++){
+                if(j >= n || words.get(j).length() <= i || words.get(j).charAt(i) != words.get(i).charAt(j))
+                    return false;
+            }
+        }
+        return true;
+    }
     
     //432. All O`one Data Structure
+
 }
 
