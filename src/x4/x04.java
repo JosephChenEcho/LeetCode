6294ddc3fba1c05ae6f71ee077516b4541126cf2
JosephChen432 <jochen@chcnetwork.org>
2017-03-06 16:01:45 -0800

    343. Integer Break

--- src/x4/x04.java
+++ src/x4/x04.java
@@ -11,6 +11,30 @@ package x4;
  */
 public class x04 {
     
+    public static void main(String[] args){
+    
+    }
+    //341. Flatten Nested List Iterator
+
+    //343. Integer Break
+    public int integerBreak(int n) {
+        if(n == 2) return 1;
+        if(n == 3) return 2;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for(int i = 4; i <= n; i++){
+            int maxp = 0;
+            int j = 1;
+            while(j <= i - j){
+                maxp = Math.max(maxp, dp[j] * dp[i - j]);                
+                j++;
+            }
+            dp[i] = maxp;
+        }
+        return dp[n];
+    }
+    
 }
 
-//341. Flatten Nested List Iterator
