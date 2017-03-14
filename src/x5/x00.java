fbda8264833e1e85efb9193446fa9bd71d19e7c6
JosephChen432 <jochen@chcnetwork.org>
2017-03-13 17:04:00 -0700

    405. Convert a Number to Hexadecimal

--- src/x5/x00.java
+++ src/x5/x00.java
@@ -116,6 +116,19 @@ public class x00 {
         }
         return ans;
     }
+    
+    //405. Convert a Number to Hexadecimal
+    public String toHex(int num) {
+        long n = num & 0x00000000ffffffffL;
+        char[] map = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            sb.insert(0, map[(int) (n % 16)]);
+            n = n / 16;
+        }
+        return num == 0 ? "0" : sb.toString();
+    }
+    
     //408. Valid Word Abbreviation
     public boolean validWordAbbreviation(String word, String abbr) {
         int i = 0, j = 0;
