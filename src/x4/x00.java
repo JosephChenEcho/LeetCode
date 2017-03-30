4cd36ecedcd1dfbc1a029885c65dbc87d82cd820
Joseph <Joseph@192.168.2.12>
2017-03-29 21:19:31 -0700

    311. Sparse Matrix Multiplication

--- src/x4/x00.java
+++ src/x4/x00.java
@@ -186,6 +186,24 @@ public class x00 {
         return leaves;*/
     }
     
+    //311. Sparse Matrix Multiplication
+    public int[][] multiply(int[][] A, int[][] B) {
+        int m = A.length, n = A[0].length, nB = B[0].length;
+        int[][] C = new int[m][nB];
+
+        for(int i = 0; i < m; i++) {
+            for(int k = 0; k < n; k++) {
+                if (A[i][k] != 0) {
+                    for (int j = 0; j < nB; j++) {
+                        if (B[k][j] != 0) C[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        }
+        return C;   
+    }
+    
+    
     //312. Burst Balloons
     public static int maxCoins(int[] iNums) {
         int[] nums = new int[iNums.length + 2];
