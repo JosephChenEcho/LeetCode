cb3deb42badb6c480b765d11a38fc2d27df35201
JosephChen432 <jochen@chcnetwork.org>
2017-03-30 16:53:06 -0700

    243. Shortest Word Distance

--- src/x3/x04.java
+++ src/x3/x04.java
@@ -80,6 +80,22 @@ public class x04 {
         return true;
     }
     
+    //243. Shortest Word Distance
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int p1 = -1;
+        int p2 = -1;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < words.length; i++){
+            if(words[i].equals(word1)) p1 = i;
+            if(words[i].equals(word2)) p2 = i;
+            if(p1 != -1 && p2 != -1){
+                min = Math.min(min, Math.abs(p1-p2));
+            }
+        }
+                
+        return min;
+    }
+    
     //244. Shortest Word Distance II
     
     //246. Strobogrammatic Number
