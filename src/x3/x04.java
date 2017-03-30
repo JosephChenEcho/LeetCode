7b146e4a2100e15c449522537969029703a4fbea
Joseph <Joseph@192.168.2.12>
2017-03-29 21:37:48 -0700

    256. Paint House

--- src/x3/x04.java
+++ src/x3/x04.java
@@ -216,6 +216,20 @@ public class x04 {
         }
     }
     
+    //256. Paint House
+    public int minCost(int[][] costs) {
+            if(costs==null||costs.length==0){
+            return 0;
+        }
+        for(int i=1; i<costs.length; i++){
+            costs[i][0] += Math.min(costs[i-1][1],costs[i-1][2]);
+            costs[i][1] += Math.min(costs[i-1][0],costs[i-1][2]);
+            costs[i][2] += Math.min(costs[i-1][1],costs[i-1][0]);
+        }
+        int n = costs.length-1;
+        return Math.min(Math.min(costs[n][0], costs[n][1]), costs[n][2]);
+    }
+    
     //257. Binary Tree Paths
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> retList = new ArrayList();
