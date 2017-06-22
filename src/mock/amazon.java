0a802e39517831aee95b356066d52b2b9a486bb1
JosephChen432 <jochen@chcnetwork.org>
2017-06-21 17:05:54 -0700

    Amazon

--- src/mock/amazon.java
+++ src/mock/amazon.java
@@ -259,6 +259,17 @@ public class amazon {
         }
         return list;
     }
+    
+    //606. Construct String from Binary Tree
+    public String tree2str(TreeNode t) {
+        if(t == null) return "";
+        if(t.left == null && t.right == null) return String.valueOf(t.val);
+        if(t.left != null && t.right != null){
+            return String.valueOf(t.val) + "(" + tree2str
+        }
+        
+    }
+    
 }
 
 
